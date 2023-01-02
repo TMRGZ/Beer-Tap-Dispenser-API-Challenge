0c90f57cb89bfc3fb64d63c033e18ab04a4b33b2
TMRGZ <miguelrg5605@outlook.es>
2023-01-02 21:54:44 +0100

    FEATURE: Created dispenser service through infrastructure layer

---
+++ src/main/java/com/rviewer/skeletons/infrastructure/config/BeanConfig.java
@@ -0,0 +1,16 @@
+package com.rviewer.skeletons.infrastructure.config;
+
+import com.rviewer.skeletons.domain.services.DispenserService;
+import com.rviewer.skeletons.domain.services.impl.DispenserServiceImpl;
+import com.rviewer.skeletons.infrastructure.persistence.repository.DispenserRepositoryImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public DispenserService dispenserService(){
+        return new DispenserServiceImpl(new DispenserRepositoryImpl());
+    }
+}
