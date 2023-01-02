d03407d5320bf7a8d362f2930c02c76fdef3cfd5
TMRGZ <miguelrg5605@outlook.es>
2023-01-02 21:55:07 +0100

    FEATURE: Added repository annotations

--- src/main/java/com/rviewer/skeletons/infrastructure/persistence/repository/JpaDispenserRepository.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/persistence/repository/JpaDispenserRepository.java
@@ -2,6 +2,8 @@ package com.rviewer.skeletons.infrastructure.persistence.repository;
 
 import com.rviewer.skeletons.infrastructure.persistence.dao.DispenserDao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaDispenserRepository extends JpaRepository<DispenserDao, String> {
 }
