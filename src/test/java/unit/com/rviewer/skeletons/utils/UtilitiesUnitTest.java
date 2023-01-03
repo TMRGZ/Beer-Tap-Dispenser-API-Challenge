788d7f9882133e9c3f2fec7ba8915d32363b6e67
TMRGZ <miguelrg5605@outlook.es>
2023-01-03 21:21:48 +0100

    FEATURE: Added unit tests for utility classes

--- src/test/java/unit/com/rviewer/skeletons/utils/UtilitiesUnitTest.java
+++ src/test/java/unit/com/rviewer/skeletons/utils/UtilitiesUnitTest.java
@@ -1,4 +1,22 @@
 package unit.com.rviewer.skeletons.utils;
 
-public class UtilitiesUnitTest {
+import com.rviewer.skeletons.utils.Utilities;
+import org.awaitility.Awaitility;
+import org.awaitility.Durations;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+class UtilitiesUnitTest {
+
+    @Test
+    void calculateTotalSpentUnitTest() {
+        Date open = new Date();
+        Awaitility.await().pollDelay(Durations.FIVE_SECONDS).until(() -> true);
+        Date close = new Date();
+
+        double totalSpent = Utilities.calculateTotalSpent(1.0, open, close);
+        Assertions.assertNotEquals(0, totalSpent);
+    }
 }
