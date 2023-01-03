2fd1de6b44c475fd8b431b282228ae4716750773
TMRGZ <miguelrg5605@outlook.es>
2023-01-03 23:46:13 +0100

    FEATURE: Changes to document error responses automatically

--- src/main/java/com/rviewer/skeletons/infrastructure/controllers/ExceptionHandlerController.java
+++ src/main/java/com/rviewer/skeletons/infrastructure/controllers/ExceptionHandlerController.java
@@ -5,23 +5,27 @@ import com.rviewer.skeletons.domain.exception.DispenserDoesNotExistException;
 import com.rviewer.skeletons.domain.exception.DispenserIsAlreadyInThatStateException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandlerController {
 
     @ExceptionHandler(BeerTapDispenserException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<Void> handleGenericException() {
         return ResponseEntity.internalServerError().build();
     }
 
     @ExceptionHandler(DispenserDoesNotExistException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Void> handleNotFoundException() {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(DispenserIsAlreadyInThatStateException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ResponseEntity<Void> handleBadStateException() {
         return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
